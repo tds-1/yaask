9936185fd2392b4e4a7e4881030827dc9a724568
tds-1 <tanmaydeepsharma21@gmail.com>
2020-03-30 05:39:31 +0530

    removes standings

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def getStandings():
 @app.route('/')
 def home():
 	print ("This is the very first page of the yaask application")
-	return render_template('index.html', users=getStandings())
+	return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
