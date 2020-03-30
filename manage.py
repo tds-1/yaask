a90dd03172d9706fa5a020406c3ca324e9946031
tds-1 <tanmaydeepsharma21@gmail.com>
2020-03-30 18:05:09 +0530

    Revert "testing again-4"
    This reverts commit cdef0d565e8c0a3045232c6b3adab399987ad137.

--- manage.py
+++ manage.py
@@ -5,15 +5,7 @@ import os
 from app import app, db
 
 
-try:
-	app.config.from_object('config.DevelopmentConfig')
-	print ("try")
-except:
-	print ("except")
-	SECRET_KEY = os.environ.get('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	WTF_CSRF_SECRET_KEY = SECRET_KEY
+app.config.from_object('config.DevelopmentConfig')
 
 migrate = Migrate(app, db)
 manager = Manager(app)
