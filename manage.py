d97023fd9e71870706a7c6f700b98c1d41f48afc
tds-1 <tanmaydeepsharma21@gmail.com>
2020-04-10 06:28:02 +0530

    manage.py fixed

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-import os
+import os import environ
 
 from app import app, db
 
@@ -9,12 +9,13 @@ try:
 	print ("try")
 except:
 	print ("except")
-	SECRET_KEY = environ['SECRET_KEY']
-	DEBUG = environ['DEBUG']
-	TESTING = environ['TESTING']
-	SQLALCHEMY_DATABASE_URI = environ['DATABASE_URL']
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	WTF_CSRF_SECRET_KEY = SECRET_KEY
+	app.config["SECRET_KEY"] = environ['SECRET_KEY']
+	app.config["DEBUG"] = True
+	app.config["TESTING"] = False
+	app.config["SQLALCHEMY_DATABASE_URI"] = environ['DATABASE_URL']
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	app.config["WTF_CSRF_SECRET_KEY"] = app.config["SECRET_KEY"]
+
 
 migrate = Migrate(app, db)
 manager = Manager(app)
